6e8affe085a7eaafebda5c74b7a00922a76464f0
Suyash Kamath <suyashkamath9@gmail.com>
2025-09-18 12:14:45 +0530

    Updated order_summary

--- 4-Loops/05_order_summary.py
+++ 4-Loops/05_order_summary.py
@@ -3,3 +3,5 @@ bills = [50, 70, 100, 55]
 
 for name, amount in zip(names, bills):
     print(f"{name} paid {amount} rupees")
+
+# Note : The list should be of same size , else the loop will end in min(all_loops)
